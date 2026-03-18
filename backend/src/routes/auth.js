c2b5b2c7bc7643c6bb8c9e8ada2aed0be76c6e27
Mankirat <mankirat2601@gmail.com>
2026-03-17 21:39:24 -0700

    Create route for /me

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -3,14 +3,18 @@ import {
     registerController,
     loginController,
     logoutController,
-    refreshController
+    refreshController,
+    getMeController
  } from '../controllers/auth.controller.js'
+import { verifyJWT } from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
 router.post('/register', registerController)
 router.post('/login', loginController)
 router.post('/logout', logoutController)
-router.post('/refresh', refreshController)  
+router.post('/refresh', refreshController) 
+
+router.get('/me', verifyJWT, getMeController)
 
 export default router
